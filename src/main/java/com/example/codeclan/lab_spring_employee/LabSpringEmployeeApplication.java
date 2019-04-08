0a4d98f0c7adcbaf6aaab29ea6acb456ee7abc7d
Nuno Farinha <nadfarinha@gmail.com>
2019-04-08 13:52:54 +0100

    Set up project with Maven

---
+++ src/main/java/com/example/codeclan/lab_spring_employee/LabSpringEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.lab_spring_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpringEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/lab_spring_employee/LabSpringEmployeeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.codeclan.lab_spring_employee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabSpringEmployeeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
