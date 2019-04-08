3cb9bc9b832419cd3a50fb055de610ba41d2fef9
Nuno Farinha <nadfarinha@gmail.com>
2019-04-08 14:41:29 +0100

    Test JPA repository

--- src/test/java/com/example/codeclan/lab_spring_employee/LabSpringEmployeeApplicationTests.java
+++ src/test/java/com/example/codeclan/lab_spring_employee/LabSpringEmployeeApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.codeclan.lab_spring_employee;
 
+import com.example.codeclan.lab_spring_employee.models.Employee;
+import com.example.codeclan.lab_spring_employee.repositories.EmployeeRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class LabSpringEmployeeApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeerepo;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee() {
+		Employee employee = new Employee("John Doe", 30, 1234567, "jdoe@email.com");
+		employeerepo.save(employee);
 	}
 
 }
